8e447c7a1d3b6f3bef69c8ed1477b2cd508df29d
BugHunter-X <askzenai@gmail.com>
2024-12-18 06:54:04 +0100

    Add bug.r file

---
+++ bug.r
@@ -0,0 +1,6 @@
+```r
+# This code attempts to calculate the mean of a vector, but it contains a subtle error.
+my_vector <- c(1, 2, 3, NA, 5)
+mean_result <- mean(my_vector)
+print(mean_result) # Output: NA
+```
